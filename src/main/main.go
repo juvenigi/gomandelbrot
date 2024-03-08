7516fcb31c95a77524a7b933e481c080629acd34
Dmytro <juvenigi@gmail.com>
2024-03-08 06:22:11 +0100

    initial commit

---
+++ src/main/main.go
@@ -0,0 +1,33 @@
+package main
+
+//
+//import (
+//	"fmt"
+//	"github.com/gopxl/pixel"
+//	"github.com/gopxl/pixel/pixelgl"
+//	"golang.org/x/image/colornames"
+//)
+//
+//func run() {
+//	cfg := pixelgl.WindowConfig{
+//		Title:  "Pixel Rocks!",
+//		Bounds: pixel.R(0, 0, 1024, 768),
+//		VSync:  true,
+//	}
+//	win, err := pixelgl.NewWindow(cfg)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	win.Clear(colornames.Skyblue)
+//
+//	fmt.Print(win.Canvas().Pixels())
+//
+//	for !win.Closed() {
+//		win.Update()
+//	}
+//}
+//
+//func main() {
+//	pixelgl.Run(run)
+//}
